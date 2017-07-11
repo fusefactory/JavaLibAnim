9eae7df1b1f54e421425e873896999c3c1ef5e03
shortnotion (mark) <shortnotion@gmail.com>
2017-07-11 10:57:01 +0200

    Manager triggers events

--- src/main/java/com/fuse/anim/Manager.java
+++ src/main/java/com/fuse/anim/Manager.java
@@ -1,5 +1,7 @@
 package com.fuse.anim;
 
+import com.fuse.utils.Event;
+import java.util.function.Consumer;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -7,9 +9,13 @@ import com.fuse.anim.Animatable;
 
 public class Manager {
 	private List<Animatable> anims;
+	public Event<Manager> doneEvent;
+	public Event<Animatable> itemDoneEvent;
 
 	public Manager(){
 		anims = new ArrayList<Animatable>();
+		doneEvent = new Event<>();
+		itemDoneEvent = new Event<>();
 	}
 
 	public void update(float dt){
@@ -19,15 +25,40 @@ public class Manager {
 			// update the active...
 			if(anim.isActive()){
 				anim.update(dt);
-				continue;
+
+				// still active? leave it, otherwise remove it
+				if(anim.isActive())
+					continue;
 			}
 
 			// ...remove the finished
+			itemDoneEvent.trigger(anim);
 			anims.remove(i);
 		}
+
+		if(isDone()){
+			doneEvent.trigger(this);
+		}
+	}
+
+	public boolean isDone(){
+		return anims.isEmpty();
 	}
 
 	public void add(Animatable anim){
 		anims.add(anim);
+	}
+
+	public Manager whenAllDone(Runnable func){
+		Consumer<Manager> consumer = (Manager man) -> {
+			func.run();
+		};
+
+		doneEvent.addListener(consumer);
+
+		if(isDone())
+			func.run();
+
+		return this;
 	}
 }
